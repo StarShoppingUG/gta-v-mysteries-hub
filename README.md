c919f416613f2930e14e559674bea4b639f1f198
StarShoppingUG <juukoalex14@gmail.com>
2025-04-30 08:02:16 +0300

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# gtaV
-# gta-v-mysteries-hub
--- mysteries/static/scripts.js
+++ mysteries/static/scripts.js
@@ -1,12 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get all navigation links
-    const navLinks = document.querySelectorAll('.nav-links a');
-    const currentPath = window.location.pathname;
+    
+    HighlightSelected();
+    ToggleMysteriesCheats();
 
-    // Add active class to current page link
-    navLinks.forEach(link => {
-        if (link.getAttribute('href') === currentPath) {
-            link.classList.add('active');
-        }
-    });
+
 });
+ //toggle mysteries and cheat codes
+function ToggleMysteriesCheats(){
+    const mysteriesBtn = document.getElementById();
+    const cheatsBtn = document.getElementById();
+    mysteriesBtn.addEventListener("click", ()=>{
+        document.querySelector("#mysteries").style.display = "block";
+        document.querySelector("#cheats").style.display = "none";
+    })
+    cheatsBtn.addEventListener("click", ()=>{
+        document.querySelector("#cheats").style.display = "block";
+        document.querySelector("#mysteries").style.display = "none";
+    })
+}
+//Highlight selected page.
+function HighlightSelected(){
+     // Get all navigation links
+     const navLinks = document.querySelectorAll('.nav-links a');
+     const currentPath = window.location.pathname;
+ 
+     // Add active class to current page link
+     navLinks.forEach(link => {
+         if (link.getAttribute('href') === currentPath) {
+             link.classList.add('active');
+         }
+      
+     });
+}
